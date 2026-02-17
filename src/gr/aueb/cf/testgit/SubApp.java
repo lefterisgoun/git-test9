cf981c0267839364214a1e85eda20432807ffb9b
Lefteris Goun <lefteris-gounaris@hotmail.com>
2026-02-17 17:48:42 +0200

    feat: add subapp

---
+++ src/gr/aueb/cf/testgit/SubApp.java
@@ -0,0 +1,12 @@
+package gr.aueb.cf.testgit;
+
+public class SubApp {
+
+    public static void main() {
+        System.out.println(subtract(10, 5));
+    }
+
+    public static int subtract(int a, int b) {
+        return a - b;
+    }
+}
